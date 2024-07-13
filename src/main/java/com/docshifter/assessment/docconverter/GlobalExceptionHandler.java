ceba16c3dbca23a1e48f60bda8b159ded689ea8c
elie.azoury <elie.azoury@euranova.eu>
2024-07-14 00:05:39 +0200

    Added exception handling

---
+++ src/main/java/com/docshifter/assessment/docconverter/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.docshifter.assessment.docconverter;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    public String handleException(Exception e) {
+        return "An error occurred: " + e.getMessage();
+    }
+}
